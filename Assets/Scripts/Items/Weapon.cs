ac6ed871e35c316ca855f2c62cba48e1c2976225
DESKTOP-VU6LCTF\khanh <khanhbklc12@gmail.com>
2018-12-06 00:19:03 +0900

    Add item class, resources manager

---
+++ Assets/Scripts/Items/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SO;
+
+namespace SA
+{
+    [CreateAssetMenu(menuName ="Items/Weapon")]
+    public class Weapon : Item
+    {
+        public int currentBullets = 30;
+        public int megazineBullets = 30;
+        public float fireRate = 0.2f;
+
+        public Vector3Variable rightHandPosition;
+        public Vector3Variable rightHandEuler;
+        public GameObject modelPrefab;
+        public RuntimeWeapon runTime;
+
+        public void Init()
+        {
+            runTime = new RuntimeWeapon();
+            runTime.modelInstance = Instantiate(modelPrefab) as GameObject;
+            runTime.weaponHook = runTime.modelInstance.GetComponent<WeaponHook>();
+            runTime.weaponHook.Init();
+        }
+
+        public class RuntimeWeapon
+        {
+            public GameObject modelInstance;
+            public WeaponHook weaponHook;
+        }
+    }
+}
---
+++ Assets/Scripts/Items/WeaponHook.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    public class WeaponHook : MonoBehaviour
+    {
+        public Transform leftHandIK;
+
+        ParticleSystem[] particles;
+
+        AudioSource audioSource;
+
+        public void Init()
+        {
+            GameObject go = new GameObject();
+            go.name = "audio holder";
+            go.transform.parent = this.transform;
+            audioSource = go.AddComponent<AudioSource>();
+            audioSource.spatialBlend = 1;
+
+            particles = GetComponentsInChildren<ParticleSystem>();
+        }
+
+        public void Shoot()
+        {
+            if(particles != null)
+            {
+                for (int i = 0; i < particles.Length; i++)
+                {
+                    particles[i].Play();
+                }
+            }
+        }
+    }
+}

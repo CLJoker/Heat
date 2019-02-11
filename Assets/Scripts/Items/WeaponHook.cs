4753bacfb82342dd6082db2106027e153c4a0787
DESKTOP-VU6LCTF\khanh <khanhbklc12@gmail.com>
2019-02-11 11:47:35 +0900

    complete weapon slider

--- Assets/Scripts/Items/WeaponHook.cs
+++ Assets/Scripts/Items/WeaponHook.cs
@@ -16,6 +16,16 @@ namespace SA
         [HideInInspector]
         public float lastFired;
 
+        public Transform slider;
+        public AnimationCurve sliderCurve;
+        public float multiplier = 1;
+        public float sliderSpeed;
+        Vector3 startPos;
+        float sliderT;
+
+        public bool isShooting;
+        bool initSliderLerp;
+
         public void Init()
         {
             GameObject go = new GameObject();
@@ -25,16 +35,47 @@ namespace SA
             audioSource.spatialBlend = 1;
 
             particles = GetComponentsInChildren<ParticleSystem>();
+
+            if(slider != null)
+                startPos = slider.localPosition;
+
         }
 
         public void Shoot()
         {
+            isShooting = true;
+
             if(particles != null)
             {
                 for (int i = 0; i < particles.Length; i++)
                 {
                     particles[i].Play();
                 }
+            }
+        }
+
+        private void Update()
+        {
+            if (isShooting)
+            {
+                if (!initSliderLerp)
+                {
+                    initSliderLerp = true;
+                    sliderT = 0;
+                }
+
+                sliderT += Time.deltaTime * sliderSpeed;
+                if(sliderT > 1)
+                {
+                    sliderT = 1;
+                    initSliderLerp = false;
+                    isShooting = false;
+                }
+
+                float targetZ = sliderCurve.Evaluate(sliderT) * multiplier;
+                Vector3 tp = startPos;
+                tp.z -= targetZ;
+                slider.transform.localPosition = tp;
             }
         }
     }

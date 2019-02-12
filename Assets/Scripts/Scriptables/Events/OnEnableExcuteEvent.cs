ec9cb4dd2936895ea791c96ebb4bd2de1b2852bb
DESKTOP-VU6LCTF\khanh <khanhbklc12@gmail.com>
2019-02-12 19:31:47 +0900

    re-add the UI scene, start refactor the UI scene

---
+++ Assets/Scripts/Scriptables/Events/OnEnableExcuteEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SA
+{
+    public class OnEnableExcuteEvent : MonoBehaviour
+    {
+        public UnityEvent onEnable;
+
+        private void Start()
+        {
+            onEnable.Invoke();
+        }
+
+    }
+}

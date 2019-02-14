2923639f5479c45f364db29d3b392c61e24f73ca
Quang <khanhbklc12@gmail.com>
2019-02-14 13:10:44 +0900

    Create connect to master server

--- Assets/Scripts/Manager/MultiplayerLauncher.cs
+++ Assets/Scripts/Manager/MultiplayerLauncher.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using SO;
 
 namespace SA
 {
-    public class MultiplayerLauncher : MonoBehaviour
+    public class MultiplayerLauncher : Photon.PunBehaviour
     {
         delegate void OnSceneLoaded();
         bool isLoading;
+        public int gameVersion = 1;
+        public PhotonLogLevel logLevel = PhotonLogLevel.ErrorsOnly;
 
         public static MultiplayerLauncher singleton;
+        public GameEvent onConnectedToMaster;
+        public BoolVariable isConnected;
 
+        #region Init
         private void Awake()
         {
             if(singleton == null)
@@ -21,7 +28,45 @@ namespace SA
             else
             {
                 Destroy(this.gameObject);
+
             }
+        }
+
+        private void Start()
+        {
+            isConnected.value = PhotonNetwork.connected;
+            ConnectToServer();
+        }
+
+        public void ConnectToServer()
+        {
+            PhotonNetwork.logLevel = logLevel;
+            PhotonNetwork.autoJoinLobby = false;
+            PhotonNetwork.automaticallySyncScene = false;
+            PhotonNetwork.ConnectUsingSettings(this.gameVersion.ToString());
+        }
+        #endregion
+
+        #region Photon Callback
+        public override void OnConnectedToMaster()
+        {
+            isConnected.value = true;
+            onConnectedToMaster.Raise();
+        }
+
+        //This maybe will be change after change Unity version
+        public override void OnFailedToConnectToPhoton(DisconnectCause cause)
+        {
+            base.OnFailedToConnectToPhoton(cause);
+            isConnected.value = false;
+        }
+
+        #endregion
+
+        #region Manager Methods
+        public void LoadMainMenu()
+        {
+            StartCoroutine(LoadScene("Main", OnMainMenu));
         }
 
         public void LoadCurrentRoom()
@@ -43,5 +88,22 @@ namespace SA
                 callback.Invoke();
             }
         }
+        #endregion
+
+        #region Setup Methods
+        public void OnMainMenu()
+        {
+            isConnected.value = PhotonNetwork.connected;
+            if (isConnected.value)
+            {
+                OnConnectedToMaster();
+            }
+            else
+            {
+                ConnectToServer();
+            }
+        }
+
+        #endregion
     }
 }

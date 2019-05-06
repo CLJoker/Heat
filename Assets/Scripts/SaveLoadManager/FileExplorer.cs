5ed09fcafca7075aa867204c59eca88900d15581
Quang <khanhbklc12@gmail.com>
2019-05-06 19:41:03 +0900

    Import FileBrowser assest, create basic UI for change player's profile

---
+++ Assets/Scripts/SaveLoadManager/FileExplorer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Crosstales.FB;
+
+public class FileExplorer : MonoBehaviour {
+
+    [HideInInspector]
+    public string path;
+    public RawImage avatar;
+
+	public void OpenImage()
+    {
+        path = FileBrowser.OpenSingleFile("", "", "png", "jpg");
+        SaveAvatar();
+    }
+
+    private void SaveAvatar()
+    {
+        WWW www = new WWW("file:///" + path);
+        avatar.texture = www.texture;
+    }
+}
